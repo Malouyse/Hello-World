6bb7988f8b39c668668a9ea1319b74ad79a1ad71
Judith Aio <Judith Aio@DESKTOP-KSD455U.fios-router.home>
2019-02-16 16:28:48 -0500

    hi I uploaded my codes

---
+++ GitProject/src/learning/git/HelloWorld.java
@@ -0,0 +1,15 @@
+package learning.git;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		
+		
+	String github = " new github Project will be fun";
+	
+	System.out.println("");
+	
+	
+	}
+}
